dfbd41cae9e42bc9e043736e1c6419df756be72c
Henrique Medeiros Dos Reis <henriquereis20180@gmail.com>
2022-02-28 15:39:43 -0600

    started the face recognition. Can now save data on it

---
+++ FaceRecognition/FaceRecognition.js
@@ -0,0 +1,6 @@
+// Syntax
+//misty.Set(string key, string value, [bool longTermStorage], [string skillUniqueId], [int prePauseMs], [int postPauseMs]);
+
+
+//misty.Set("name", 9, true, "2f378334-4caf-4af8-9cd4-d9ae3d06304c", 1,1);
+misty.Debug(misty.Get("name"));
