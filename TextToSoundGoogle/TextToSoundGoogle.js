c3b9a8879dcce99d779ab2ea6fbc662a9d6961b8
Henrique Medeiros Dos Reis <henriquereis20180@gmail.com>
2022-03-28 13:18:58 -0500

    tested and it is working. just missing communication

--- TextToSoundGoogle/TextToSoundGoogle.js
+++ TextToSoundGoogle/TextToSoundGoogle.js
@@ -2,6 +2,7 @@
 // 1. Get an API Key from Google Cloud Platform Console for TTS and update APIKEY_Google in the .JSON file
 // Henrique Can help you with that, and I woulndt share your key with other people, but thats up to you
 
+//change this string to the desired text
 speakTheText("test");
 
 // ==================== Send Text To Google ===========================
@@ -33,11 +34,8 @@ function speakTheText(text) {
 // ==================== Audio returned from Google ====================
 
 function _Base64In(data) {
-    try {
         misty.SaveAudio("TTS.wav", JSON.parse(data.Result.ResponseObject.Data).audioContent, true, true);
-    } catch (error) {
-        misty.Debug(JSON.stringify(data));
-    }
+    
 }
 
 // ============= Enable REST API call directly to skill ================
@@ -45,12 +43,7 @@ function _Base64In(data) {
 misty.RegisterUserEvent("speakTheText", true);
 
 function _speakTheText(data) {
-    try {
-        misty.Debug(JSON.stringify(data));
+        //misty.Debug(JSON.stringify(data));
         speakTheText(data.text);
-    } catch (error) {
-        misty.Debug("Cannot parse text input to speakTheText(); use this format { \"text\" : \"Your Text\"");
-    }   
 }
-
-//needs testing, but should be pretty straight foward 
+ //working, just missing communication to the other robot
