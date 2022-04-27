3576c7bb89e5e2f0fc4ddd415308d173c731cba8
gabiMiranda <gabi.miranda15@hotmail.com>
2022-04-27 15:00:09 -0500

    added testDashboard

---
+++ testDashboard/testDashboard.js
@@ -0,0 +1,11 @@
+/*let arguments = "test";
+misty.SendExternalRequest("POST", "http://10.12.132.87:48000/api/SetTaskInfo", null, null,JSON.stringify(arguments), false, false, null, "application/json","{}");
+
+
+
+misty.SendExternalRequest("GET", "http://10.12.132.87:48000/api/GetTaskInfo", null, null, null, false, false, null, "application/json","_return")
+
+function _return(data) {
+    misty.Debug(JSON.stringify(data.Result.ResponseObject.Data));
+
+}*/
